05afe0e65b3ca586d2a620a8c0b1d95f2a1cbcf2
MKN2003 <kazimovmiraziz197@gmail.com>
2023-06-08 14:29:40 +0500

    Changes have been made

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -3,13 +3,14 @@ package school.mjc.stage0.loops.task4;
 public class FixMe {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-            } else if (i == 7) {
+            if (i % 2 != 0) {
+                continue;
+            } else if (i == 8) {
                 System.out.println("last");
-                break;
                 System.out.println("will I be printed?");
+                break;
             }
+            System.out.println(i);
         }
     }
 }
